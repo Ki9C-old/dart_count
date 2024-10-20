47e8c9309544a9e1efbcdde6e42d4edd3135a187
katapote <ri9.jp313@gmail.com>
2024-10-20 14:37:13 +0900

    ZeroOneを追加

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ player_score = {
 }
 current_player = "Player1"
 dart_left = int(3)
-
+mode = str('')
 score_history = []
+winner = 'Null'
 
 #################初期画面#####################
 @app.route('/',methods=['GET','POST'])
@@ -24,6 +25,7 @@ def select_mode():
     global player_score
     global score_history
     global dart_left
+    global mode
 
     current_player = "Player1"
     player_score = {
@@ -36,7 +38,8 @@ def select_mode():
     mode = request.form.get('mode')
     if mode == 'CountUp':
         return CountUp()
-    #if mode == 'ZeroOne':...
+    elif mode == 'ZeroOne':
+        return ZeroOne()
 
 
 #################ボードクリック計算#####################
@@ -52,7 +55,23 @@ def calculate():
         return render_template('CountUp.html', player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left)
     else:
         return render_template('CountUp.html', player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left, flg=True)
-    
+
+
+@app.route('/cal_ZeroOne', methods=['POST'])
+def calculateZeroOne():
+    global current_player
+    global dart_left
+    global winner
+    if dart_left > 0:
+        dart_left = dart_left-1
+        thisPoint = int(request.form.get('score'))
+        score_history.append((current_player, thisPoint))
+        player_score[current_player] -= thisPoint
+        if player_score[current_player] == 0:
+            winner = current_player
+        return render_template('ZeroOne.html', player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left,winner=winner)
+    else:
+        return render_template('ZeroOne.html', player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left, flg=True)
 
 
 #################NextPlayer#####################
@@ -60,12 +79,15 @@ def calculate():
 def next_player():
     global current_player
     global dart_left
+    global mode
+
+    gamemode="{}.html".format(mode)
     dart_left = int(3)
     if current_player =="Player1":
         current_player = "Player2"
     else:
         current_player = "Player1"
-    return render_template('CountUp.html', player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left)
+    return render_template(gamemode, player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left)
 
 
 #################取り消しボタン#####################
@@ -73,17 +95,39 @@ def next_player():
 def undo():
     global current_player
     global dart_left
-    if score_history and dart_left < 3:
-        dart_left = dart_left + 1
-        last_score = score_history.pop()
-        player,thisPoint = last_score
-        player_score[player] -= thisPoint
-    return render_template('CountUp.html', player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left)
+    global mode
+
+    gamemode = "{}.html".format(mode)
+    if mode == 'CountUp': 
+        if score_history and dart_left < 3:
+            dart_left = dart_left + 1
+            last_score = score_history.pop()
+            player,thisPoint = last_score
+            player_score[player] -= thisPoint
+    
+    if mode == 'ZeroOne':
+        if score_history and dart_left < 3:
+            dart_left = dart_left + 1
+            last_score = score_history.pop()
+            player,thisPoint = last_score
+            player_score[player] += thisPoint
+    return render_template(gamemode, player1_score=player_score["Player1"], player2_score=player_score["Player2"],current_player=current_player,dart_left=dart_left)
 
 
 #################カウントアップ初期表示#####################
 def CountUp():
     return render_template('CountUp.html', player1_score=0, player2_score=0,current_player=current_player,dart_left=dart_left)
+
+
+###############################ゼロワン#####################################
+def ZeroOne():
+    global player_score
+    global dart_left
+    player_score = {
+        "Player1": 701,
+        "Player2": 701
+    }
+    return render_template('ZeroOne.html', player1_score=player_score['Player1'], player2_score=player_score['Player2'],current_player=current_player,dart_left=dart_left)
 
 
 if __name__ == '__main__':
